89062a580a400d8e4b28ce837ebda1f3732d2fde
Pritom Chaki <pritom.cse.diu@gmail.com>
2021-03-13 15:49:05 +0600

    Optimized

--- e_commerce_app/lib/screens/details_screen.dart
+++ e_commerce_app/lib/screens/details_screen.dart
@@ -36,6 +36,200 @@ class _DetailScreenState extends State<DetailScreen> {
     );
   }
 
+  Widget _buildProductImage() {
+    return Center(
+      child: Container(
+        width: 350,
+        //color: Colors.blue,
+        child: Card(
+          child: Container(
+            padding: EdgeInsets.all(13),
+            child: Container(
+              height: 220,
+              decoration: BoxDecoration(
+                // color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage("assets/images/${widget.productImage}"),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNamePrice() {
+    return Column(
+      children: [
+        Container(
+          //color: Colors.red,
+          height: 100,
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.productName,
+                    style: productHeadTextStyle(),
+                  ),
+                  Text(
+                    "\$ ${widget.productPrice.toString()}",
+                    style: productHeadTextStyle(),
+                  ),
+                  Text(
+                    "Description",
+                    style: productHeadTextStyle(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 200,
+          // color: Colors.blue,
+          child: Wrap(
+            children: [
+              Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProductSizeHead() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Size",
+          style: productHeadTextStyle(),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          width: 250,
+          color: Colors.grey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildProductSize(sizeTitle: "S"),
+              _buildProductSize(sizeTitle: "M"),
+              _buildProductSize(sizeTitle: "L"),
+              _buildProductSize(sizeTitle: "XL"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProductColorHead() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Color",
+          style: productHeadTextStyle(),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          width: 250,
+          //color: Colors.grey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildProductColor(productColor: Colors.green[200]),
+              _buildProductColor(productColor: Colors.blue[200]),
+              _buildProductColor(productColor: Colors.red[200]),
+              _buildProductColor(productColor: Colors.yellow[200]),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildQuantity() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Quantity",
+          style: productHeadTextStyle(),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 30,
+          width: 130,
+          decoration: BoxDecoration(
+            color: Colors.cyan,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                child: Icon(Icons.remove),
+                onTap: () {
+                  setState(() {
+                    if (count > 1) {
+                      count--;
+                    }
+                  });
+                },
+              ),
+              Text(
+                count.toString(),
+                style: productHeadTextStyle(),
+              ),
+              GestureDetector(
+                child: Icon(Icons.add),
+                onTap: () {
+                  setState(() {
+                    count++;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton() {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      // color: Colors.black,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Colors.pinkAccent,
+        onPressed: () {},
+        child: Text(
+          "Check Out",
+          style: productHeadTextStyle(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,188 +263,28 @@ class _DetailScreenState extends State<DetailScreen> {
         ],
       ),
       body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
         child: ListView(
           children: [
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Center(
-                  child: Container(
-                    width: 350,
-                    //color: Colors.blue,
-                    child: Card(
-                      child: Container(
-                        padding: EdgeInsets.all(13),
-                        child: Container(
-                          height: 220,
-                          decoration: BoxDecoration(
-                            // color: Colors.red,
-                            image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: AssetImage(
-                                  "assets/images/${widget.productImage}"),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                _buildProductImage(),
+                _buildNamePrice(),
+                _buildProductSizeHead(),
+                SizedBox(
+                  height: 15,
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        //color: Colors.red,
-                        height: 100,
-                        child: Row(
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.productName,
-                                  style: productHeadTextStyle(),
-                                ),
-                                Text(
-                                  "\$ ${widget.productPrice.toString()}",
-                                  style: productHeadTextStyle(),
-                                ),
-                                Text(
-                                  "Description",
-                                  style: productHeadTextStyle(),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        // color: Colors.blue,
-                        child: Wrap(
-                          children: [
-                            Text(
-                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                              style: TextStyle(
-                                fontSize: 15,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Text(
-                        "Size",
-                        style: productHeadTextStyle(),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        width: 250,
-                        color: Colors.grey,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            _buildProductSize(sizeTitle: "S"),
-                            _buildProductSize(sizeTitle: "M"),
-                            _buildProductSize(sizeTitle: "L"),
-                            _buildProductSize(sizeTitle: "XL"),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        "Color",
-                        style: productHeadTextStyle(),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        width: 250,
-                        //color: Colors.grey,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            _buildProductColor(productColor: Colors.green[200]),
-                            _buildProductColor(productColor: Colors.blue[200]),
-                            _buildProductColor(productColor: Colors.red[200]),
-                            _buildProductColor(
-                                productColor: Colors.yellow[200]),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "Quantity",
-                        style: productHeadTextStyle(),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 30,
-                        width: 130,
-                        decoration: BoxDecoration(
-                          color: Colors.cyan,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            GestureDetector(
-                              child: Icon(Icons.remove),
-                              onTap: () {
-                                setState(() {
-                                  if (count > 1) {
-                                    count--;
-                                  }
-                                });
-                              },
-                            ),
-                            Text(
-                              count.toString(),
-                              style: productHeadTextStyle(),
-                            ),
-                            GestureDetector(
-                              child: Icon(Icons.add),
-                              onTap: () {
-                                setState(() {
-                                  count++;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 60,
-                        width: double.infinity,
-                        // color: Colors.black,
-                        child: RaisedButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          color: Colors.pinkAccent,
-                          onPressed: () {},
-                          child: Text(
-                            "Check Out",
-                            style: productHeadTextStyle(),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                _buildProductColorHead(),
+                SizedBox(
+                  height: 10,
                 ),
+                _buildQuantity(),
+                SizedBox(
+                  height: 10,
+                ),
+                _buildButton(),
               ],
             ),
           ],
